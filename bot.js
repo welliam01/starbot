768a8c3c06b21a6cd979b5b3d53e1aab86087dc1
yousseftange17 <46229103+yousseftange17@users.noreply.github.com>
2019-01-19 09:14:53 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const ms = require("ms");
 const fs = require('fs');
 const sw = JSON.parse(fs.readFileSync(`./setwlc.json`, `utf8`))
 
-const prefix = ","
+const prefix = "-"
 
 
 
@@ -13,38 +13,38 @@ client.on('message', message => {
           if (!message.content.startsWith(prefix)) return;
           var args = message.content.split(' ').slice(1);
           var argresult = args.join(' ');
-          if (message.author.id == 470451682417967116) return;
+          if (message.author.id == 535571590763773954) return;
 
 
         if (message.content.startsWith(prefix + 'playing')) {
-        if (message.author.id !== '465110270939889665') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
+        if (message.author.id !== '535571590763773954') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
         client.user.setGame(argresult);
             message.channel.sendMessage(`**${argresult}** : تم تغيير الحالة`)
         } else
 
 
         if (message.content.startsWith(prefix + 'streem')) {
-        if (message.author.id !== '465110270939889665') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
+        if (message.author.id !== '535571590763773954') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
         client.user.setGame(argresult, "http://twitch.tv/y04zgamer");
             message.channel.sendMessage(`**${argresult}** :تم تغيير الحالة الى ستريمنج`)
         } else
 
         if (message.content.startsWith(prefix + 'setname')) {
-        if (message.author.id !== '465110270939889665') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
+        if (message.author.id !== '535571590763773954') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
           client.user.setUsername(argresult).then
               message.channel.sendMessage(`**${argresult}** : تم تغير الأسم`)
           return message.reply("**لا تستطيع تغير الأسم الا بعد ساعتين**");
         } else
 
         if (message.content.startsWith(prefix + 'setavatar')) {
-        if (message.author.id !== '465110270939889665') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
+        if (message.author.id !== '535571590763773954') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
         client.user.setAvatar(argresult);
             message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
         } else
 
 
         if (message.content.startsWith(prefix + 'watching')) {
-        if (message.author.id !== '465110270939889665') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
+        if (message.author.id !== '535571590763773954') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
             client.user.setActivity(argresult, {type : 'watching'});
          message.channel.sendMessage(`**${argresult}** : تم تغيير الووتشينق الى`)
         }
